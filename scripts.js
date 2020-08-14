01ac7e4eef566726423647c67293a3929b7591a1
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-14 11:04:37 -0700

    Refactor javascript to correctly handle document.ready events and form submit events. Remove unnecessary IDs from form elements.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-$(document).ready(function{
-  const beverage = $("#beverage").val();
-  const timeofday = $("#timeofday").val;
-  const typeofapplications = $("#typeofapplications").val();
+$(document).ready(function () {
+  $("form").submit(function () {
+    const beverage = $("#beverage").val();
+    const timeofday = $("#timeofday").val;
+    const typeofapplications = $("#typeofapplications").val();
+    alert(beverage);
+  });
+
+
 });
+/* Function to change answer if input changes */

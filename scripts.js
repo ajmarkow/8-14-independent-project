e703cfdb1812a4e30c2e92f8b2fca87bb469866e
Alexander Markow <alexanderjmarkow@gmail.com>
2020-08-14 17:01:50 -0700

    Update language suggestion logic branching.

--- scripts.js
+++ scripts.js
@@ -1,28 +1,27 @@
 $(document).ready(function () {
   $("form").submit(function () {
-    const beverage = string($("#beverage").val());
-    const timeofday = $("#timeofday").val());
-  const typeofapplications = $("#typeofapplications").val());
-const macorpc = $("#operatingsystem").val();
-const favoritecolor = $("#favoritecolor").val();
-alert(favoritecolor + macorpc)
-if ($(macorpc === "mac")) {
-  $('#theanswer').text("Swift!");
-}
-else {
-  $('#theanswer').text("Python!");
-}
-event.preventDefault();
+    const beverage = $("#beverage").val();
+    const timeofday = $("#timeofday").val();
+    const typeofapplications = string$("#typeofapplications").val();
+    const macorpc = $("#operatingsystem").val();
+    const favoritecolor = $("#favoritecolor").val();
+    alert(favoritecolor + macorpc)
+    if ($(macorpc === "mac")) {
+      $('#theanswer').text("Swift!");
+    } else if ($(macorpc === "pc")) {
+      $('#theanswer').text("Python!");
+    }
+    event.preventDefault();
   });
 
-$('#darktoggle').change(function () {
-  if ($('#darktoggle').is(':checked')) {
-    $('body').addClass("inverted");
-  }
-  else if ($('#darktoggle').is !== (':checked')) {
-    $('body').removeClass("inverted");
-  }
-});
+  $('#darktoggle').change(function () {
+    if ($('#darktoggle').is(':checked')) {
+      $('body').addClass("inverted");
+    }
+    else if ($('#darktoggle').is !== (':checked')) {
+      $('body').removeClass("inverted");
+    }
+  });
 });
 
 
